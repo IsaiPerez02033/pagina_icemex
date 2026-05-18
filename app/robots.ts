71fbfe50f75d6c70d1c3e678409b38d4e5b9bb99
Isai Aram Pérez Flores <139027393+IsaiPerez02033@users.noreply.github.com>
2026-05-18 13:26:44 -0600

    fix: robots.txt allow /_next/image, Product schema add lowPrice/highPrice + image, remove sandbox from image CSP

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,13 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/api/", "/_next/"],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: ["/", "/_next/image/"],
+        disallow: ["/api/", "/_next/static/"],
+      },
+    ],
     sitemap: "https://icemex.mx/sitemap.xml",
   };
 }
